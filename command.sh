f96f7eb075e098777d793d6d10c6533fffd585b9
GaripovAynur <kuyan911@gmail.com>
2025-01-29 13:57:44 +0300

    Create command.sh

---
+++ command.sh
@@ -0,0 +1,14 @@
+yc config profile list # Какие есть профили
+yc config list # Показывает, какие конфиги есть у пользователя token, cloud-id, folder-id
+
+##Перед запуском terraform
+export YC_TOKEN=$(yc iam create-token)
+export YC_CLOUD_ID=$(yc config get cloud-id)
+export YC_FOLDER_ID=$(yc config get folder-id)
+#####
+
+##Задайте конфигурацию профил
+yc config set service-account-key <загруженный_ключ>
+yc config set cloud-id <идентификатор_облака>
+yc config set folder-id <идентификатор_каталога>
+####
